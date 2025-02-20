4a178b1f5465c8c95fb1d272d44bf3fa83679bb1
makintosh1337 <zxcirclet@outlook.com>
2025-02-21 03:06:11 +0700

    666

--- js/main.js
+++ js/main.js
@@ -9,19 +9,32 @@ let app = new Vue({
         inventory: 100,
         onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-
+        cart: 0,
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg"
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg"
             }
         ],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-
-        onSale: true
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        removeFromCart() {
+            if (this.cart > 0) {
+                this.cart -= 1;
+            }
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage;
+        }
     }
 });
