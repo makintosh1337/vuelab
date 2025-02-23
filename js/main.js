d61ed7946811b5902e45aaa50743df12d222060b
makintosh1337 <zxcirclet@outlook.com>
2025-02-24 02:31:19 +0700

    999

--- js/main.js
+++ js/main.js
@@ -1,102 +1,3 @@
-Vue.component('product', {
-    props: {
-        premium: {
-            type: Boolean,
-            required: true
-        }
-    },
-    template: `
-        <div class="product">
-            <div class="product-image">
-                <img :src="image" :alt="altText"/>
-            </div>
-            <div class="product-info">
-                <h1>{{ title }}</h1>
-                <p v-if="inStock">In Stock</p>
-                <p v-else :class="{ outOfStock: !inStock }">Out of Stock</p>
-                <p>Shipping: {{ shipping }}</p>
-                <product-details :details="details"></product-details>
-                <div
-                    class="color-box"
-                    v-for="(variant, index) in variants"
-                    :key="variant.variantId"
-                    :style="{ backgroundColor: variant.variantColor }"
-                    @mouseover="updateProduct(index)"
-                ></div>
-            </div>
-            <div class="cart">
-                <p>Cart ({{ cart }})</p>
-                <button
-                    @click="addToCart"
-                    :disabled="!inStock"
-                    :class="{ disabledButton: !inStock }"
-                >
-                    Add to cart
-                </button>
-                <button @click="removeFromCart">Remove from cart</button>
-            </div>
-        </div>
-    `,
-    data() {
-        return {
-            product: "Socks",
-            brand: 'Vue Mastery',
-            selectedVariant: 0,
-            altText: "A pair of socks",
-            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-            variants: [
-                {
-                    variantId: 2234,
-                    variantColor: 'green',
-                    variantImage: "./assets/vmSocks-green-onWhite.jpg",
-                    variantQuantity: 10
-                },
-                {
-                    variantId: 2235,
-                    variantColor: 'blue',
-                    variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-                    variantQuantity: 0
-                }
-            ],
-            cart: 0
-        };
-    },
-    methods: {
-        addToCart() {
-            this.cart += 1;
-        },
-        removeFromCart() {
-            if (this.cart > 0) {
-                this.cart -= 1;
-            }
-        },
-        updateProduct(index) {
-            this.selectedVariant = index;
-        }
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product;
-        },
-        image() {
-            return this.variants[this.selectedVariant].variantImage;
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].variantQuantity > 0;
-        },
-        shipping() {
-            return this.premium ? "Free" : 2.99;
-        }
-    }
-});
-
-let app = new Vue({
-    el: '#app',
-    data: {
-        premium: true
-    }
-});
-
 Vue.component('product-details', {
     props: {
         details: {
@@ -176,12 +77,10 @@ Vue.component('product', {
     },
     methods: {
         addToCart() {
-            this.cart += 1;
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId); // Генерация события
         },
         removeFromCart() {
-            if (this.cart > 0) {
-                this.cart -= 1;
-            }
+            this.$emit('remove-from-cart', this.variants[this.selectedVariant].variantId); // Генерация события
         },
         updateProduct(index) {
             this.selectedVariant = index;
@@ -199,6 +98,25 @@ Vue.component('product', {
         },
         shipping() {
             return this.premium ? "Free" : 2.99;
+        }
+    }
+});
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        premium: true,
+        cart: []
+    },
+    methods: {
+        updateCart(id) {
+            this.cart.push(id);
+        },
+        removeFromCart(id) {
+            const index = this.cart.indexOf(id);
+            if (index !== -1) {
+                this.cart.splice(index, 1);
+            }
         }
     }
 });
